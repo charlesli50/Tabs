1d98960671d4a17686617687ec8a293cd3e34e6e
Charles Li <charlesli@newtonma.gov>
2019-07-15 14:47:03 -0400

    added validation for page 2, and working on displaying data

--- main.js
+++ main.js
@@ -8,33 +8,21 @@ $( document ).ready(function(){
 
 // on tab1 click, hide tab 1 and show tab 2
 
-$ ( '#tab1').click(function(){
-	$( '#content2').hide()
-	$( '#content1').show()
-})
-
-$ ("#submit1").click(function(){
-	$( '#content1').hide()
-	$( '#content2').show()
-	$( '.part1').css("background-color", "#ddd")
-	$( '.part1').css("color", "black")
-	$( '.part2').css("background-color", "#ccc")
-	$( '.part2').css("color", "#4287f5")
-})
-
 // if valid move onto part2
 
 var form1 = $( "#form1" )
 
+// part 1 validation
 
 form1.validate();	
 function isValid(){
 	if (form1.valid() === true){
 		part2()
+		data(form1)
 	}
 }
 
-
+// move to part 2
 
 function part2(){
 	$( '#content1').hide()
@@ -45,8 +33,36 @@ function part2(){
 	$( '.part2').css("color", "#4287f5")
 }
 
-
-$("#form1").submit(function(e) {
+// prevent reload on submit
+$("form").submit(function(e) {
     e.preventDefault();
 });
  
+
+//part 2 validation
+
+var form2 = $('#form2')
+
+form2.validate();
+
+function isValid2(){
+	if (form2.valid() === true){
+		data(form2)
+		display()
+	}
+}
+
+//display info from the 2 forms
+
+var info = {}
+
+
+function data(form){
+	data = form.serializeArray()
+	info += data
+	console.log(info)
+}
+
+function display(){
+	$("#info").html(JSON.stringify(info))
+}
